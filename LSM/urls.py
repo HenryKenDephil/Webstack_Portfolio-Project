3593e7382578507911ca3e3603fc44cb69488c01
HenryKenDePhil <h.komoloh@gmail.com>
2023-04-14 14:36:56 +0300

    adding templates

--- LSM/urls.py
+++ LSM/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('courses/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 
