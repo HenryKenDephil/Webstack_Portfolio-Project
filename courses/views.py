058e5c297752f296d312ab306de23a007b72853c
HenryKenDePhil <h.komoloh@gmail.com>
2023-04-15 18:04:31 +0300

    created multiple class inheritance via maxins and user groups

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Course
+from django.views.generic.edit import CreateView, \
+    UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
+
+class ManageCourseView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+    
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+    
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+
+class OwnerCourseMixin:
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'course/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseEditMixin, ListView):
+    template_name = 'course/manage/course/list.html'
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
+    template_name = 'course/manage/course/delete.html'
