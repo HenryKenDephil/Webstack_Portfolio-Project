1a7b5b3c9df7f22f8623a54087bc2b4a54c8421a
HenryKenDePhil <h.komoloh@gmail.com>
2023-04-16 11:21:24 +0300

    created content module

--- LSM/urls.py
+++ LSM/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('/', include('courses.urls')),
+    path('courses/', include('courses.urls')),
 ]
 
 
